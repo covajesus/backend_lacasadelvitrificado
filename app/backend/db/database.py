65c46a976a868cc9c7507a36a948eca102ef7eb3
Jesus Rafael Cova Huerta <jesus@payku.com>
2025-08-14 00:20:46 -0400

    cambios

--- app/backend/db/database.py
+++ app/backend/db/database.py
@@ -6,7 +6,14 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://admin:Admin2020!@31.97.250.169:3306/lacasadelvitrificado"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URI, pool_size=20, max_overflow=0, echo=False)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URI,
+    pool_size=200,           # más conexiones simultáneas
+    max_overflow=200,        # conexiones extra si se saturan las 200
+    pool_timeout=60,        # segundos que espera antes de dar timeout
+    pool_recycle=3600,      # reciclar conexiones cada hora
+    echo=False
+)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
